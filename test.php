7a4a58089d6e07ff2443ba86929853481afb9261
ganggangdit <1158303912@qq.com>
2015-01-06 18:15:43 +0800

    fenzhi

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php 
-   echo "This is my fist-page for the framework on github";
+   echo "This is my fist-page for the framework on github";//zhuxian
    //Todo  Test git status;
    //Todo it's remote branch_1
 
